e686ef716911fd1344964092b157dd0c9a57032c
Sai Vignesh Abishek <sai@svabishek.com>
2026-05-31 01:05:19 +0200

    add TinyWMDataset

---
+++ dataset.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import h5py
+import torch
+from random import Random
+from torch.utils.data import Dataset
+
+class TinyWMDataset(Dataset):
+    """returns frames and actions"""
+    def __init__(self, path="data/smb_30fps_256x240.h5", window_len=64,  stride=1, train=True, n_val_episodes=25, seed=404):
+   
+        self.h5_path = Path(path)
+        self.window_len = window_len
+        self.split = "train" if train else "val"
+        self._h5 = None
+        self.sample_indices = []
+
+        with h5py.File(self.h5_path, 'r') as h5:
+            episode_offsets = h5["episode_offsets"][:]
+
+        num_episodes = len(episode_offsets) - 1
+        episode_ids = [i for i in range(num_episodes)]
+        Random(seed).shuffle(episode_ids)
+
+        if self.split == "train":
+            split_episode_ids = episode_ids[n_val_episodes:]
+        else:
+            split_episode_ids = episode_ids[:n_val_episodes]
+
+        for idx in split_episode_ids:
+            start = int(episode_offsets[idx])
+            end = int(episode_offsets[idx + 1])
+            for i in range(start, end - self.window_len + 1, stride):
+                self.sample_indices.append((i, i+self.window_len))
+
+    def __getitem__(self, idx):
+        if self._h5 is None:
+            self._h5 = h5py.File(self.h5_path, 'r')
+        start, end = self.sample_indices[idx]
+        frames = self._h5["frames"][start:end]
+        frames = torch.from_numpy(frames).permute(0,3,1,2).float() / 255.0 # (T, 3, H, W)
+        actions = torch.from_numpy(self._h5["action"][start:end]).long() # (T,)
+        return frames, actions
+    
+    def __len__(self):
+        return len(self.sample_indices)
